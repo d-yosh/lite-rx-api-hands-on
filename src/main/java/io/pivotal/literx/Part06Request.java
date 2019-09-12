740aaec2b51d949b58e8121dd9198769d6399712
d-yosh <daicedaice87@gmail.com>
2019-09-12 10:26:27 +0900

    solve Part06

--- src/main/java/io/pivotal/literx/Part06Request.java
+++ src/main/java/io/pivotal/literx/Part06Request.java
@@ -3,6 +3,8 @@ package io.pivotal.literx;
 import io.pivotal.literx.domain.User;
 import io.pivotal.literx.repository.ReactiveRepository;
 import io.pivotal.literx.repository.ReactiveUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
@@ -13,34 +15,46 @@ import reactor.test.StepVerifier;
  */
 public class Part06Request {
 
-	ReactiveRepository<User> repository = new ReactiveUserRepository();
+    ReactiveRepository<User> repository = new ReactiveUserRepository();
 
 //========================================================================================
 
-	// TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
-	StepVerifier requestAllExpectFour(Flux<User> flux) {
-		return null;
-	}
+    // TODO Create a StepVerifier that initially requests all values and expect 4 values to be received
+    StepVerifier requestAllExpectFour(Flux<User> flux) {
+        return StepVerifier.create(flux)
+                .expectNextCount(4)
+                .expectComplete();
+    }
 
 //========================================================================================
 
-	// TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE.
-	StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
-		return null;
-	}
+    // TODO Create a StepVerifier that initially requests 1 value and expects User.SKYLER then requests another value and expects User.JESSE.
+    StepVerifier requestOneExpectSkylerThenRequestOneExpectJesse(Flux<User> flux) {
+        return StepVerifier.create(flux)
+                .thenRequest(1)
+                .expectNext(User.SKYLER)
+                .thenRequest(1)
+                .expectNext(User.JESSE)
+                .thenCancel();
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
-	Flux<User> fluxWithLog() {
-		return null;
-	}
+    // TODO Return a Flux with all users stored in the repository that prints automatically logs for all Reactive Streams signals
+    Flux<User> fluxWithLog() {
+        return repository.findAll().log();
+    }
 
 //========================================================================================
 
-	// TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
-	Flux<User> fluxWithDoOnPrintln() {
-		return null;
-	}
+    // TODO Return a Flux with all users stored in the repository that prints "Starring:" on subscribe, "firstname lastname" for all values and "The end!" on complete
+    Flux<User> fluxWithDoOnPrintln() {
+        Logger logger = LoggerFactory.getLogger(Part06Request.class);
+        return repository.findAll()
+                .doOnSubscribe(s -> logger.info("Starring:"))
+                .doOnNext(u -> logger.info("{} {}", u.getFirstname(), u.getLastname()))
+                .doOnComplete(() -> logger.info("The end!"));
+
+    }
 
 }
